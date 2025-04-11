52e6acb100fab38ff388909a7952f610b89e4e6f
Yusifd <yusifdavidov@mail.ru>
2025-04-11 12:13:41 +0400

    front-9.5

--- home-project/src/ContactInfo.jsx
+++ home-project/src/ContactInfo.jsx
@@ -4,17 +4,17 @@ function ContactInfo(){
     return(
         <>
         <div className="flex justify-center rounded-xl">
-            <div className="shadow-xl pt-9 pb-9 pl-7 pr-7">
+            <div className="shadow-xl pt-9 pb-9 pl-7 pr-7 rounded-[10px] w-[50%]">
                 <img src={phoneIcon}/>
-                <h6>
+                <h6 className="font-bold">
                     010-020-0340
                     <br/>
                     <span>Phone Number</span>
                 </h6>
             </div>
-            <div className="shadow-xl">
+            <div className="shadow-xl pt-9 pb-9 pl-7 pr-7 rounded-[10px] w-[50%]">
                 <img src={emailIcon}/>
-                <h6>
+                <h6 className="font-bold">
                     info@villa.com
                     <br/>
                     <span>Bussines Email</span>
